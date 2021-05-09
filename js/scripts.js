34539ef0f2ba74ef53de6086ce46e9510718b76b
Dorian <dorianoriginaldesings@gmail.com>
2021-05-09 02:12:26 +0200

    Menu vertical responsive

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,10 @@ toggleMenu.addEventListener('click', () => {
   mainMenu.classList.toggle('main-menu--show');
   toggleMenu.classList.toggle('toggle-menu--show');
 });
+
+window.addEventListener('resize', () => {
+  if (mainNav.classList.contains('nav--show')) {
+    mainNav.classList.remove('nav--show');
+    mainMenu.classList.remove('main-menu--show');
+  }
+});
